3c46aa388b1f21217b3455eb8382e1dd54449a03
Cadu mazalli <carlosemdl@gmail.com>
2026-04-16 02:54:56 -0300

    Ajustes visuais

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import '../context/styles/Footer.css';
 function Footer() {
     return (
         <footer className='footer'>
-            <p>Carlos Lopes e Bruno Dutra © 2026 Bananinis. Feito com ❤️.</p>
+            <p>Carlos Lopes e Bruno Dutra © 2026 Buscador da Câmara. Feito com ❤️.</p>
         </footer>
     );
 }
